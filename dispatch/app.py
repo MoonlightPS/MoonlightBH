2f889266399e2d88e59194f71d94386e9220e9c0
tamilpp25 <tamilpp25@gmail.com>
2022-12-05 09:33:15 +0530

    Fix dispatch? nope

--- dispatch/app.py
+++ dispatch/app.py
@@ -5,9 +5,11 @@ import base64
 
 app = Bottle()
 
-app.route('/')
+
+@app.route('/')
 def running():
     return "Running on MoonlightBH"
+
 
 ACCOUNT_INFO = {
     "retcode": 0,
@@ -17,8 +19,8 @@ ACCOUNT_INFO = {
             "uid": "1",
             "name": "MoonlightBH",
             "email": "MoonlightBH",
-            "mobile": "",
-            "is_email_verify": 1,
+            "mobile": "420****69",
+            "is_email_verify": "0",
             "realname": "",
             "identity_card": "",
             "token": "token",
@@ -30,23 +32,196 @@ ACCOUNT_INFO = {
             "apple_name": "",
             "sony_name": "",
             "tap_name": "",
-            "country": "US",
+            "country": "CN",
             "reactivate_ticket": "",
-            "area_code": "US",
+            "area_code": " * *",
             "device_grant_ticket": "",
-            "steam_name": "",
+            "steam_name": ""
         },
         "device_grant_required": False,
-        "safe_mobile_required": False,
+        "safe_moblie_required": False,
         "realperson_required": False,
-        "realname_operation": "None",
-    },
+        "reactivate_required": False,
+        "realname_operation": "None"
+    }
 }
+
+# Auth Handlers
 
 
 @app.route("/bh3_usa/mdk/shield/api/login", method="POST")
 def route_mdk_shield_api_login():
     return ACCOUNT_INFO
+
+
+@app.route("/bh3_usa/mdk/shield/api/verify", method="POST")
+def route_mdk_shield_api_verify():
+    return ACCOUNT_INFO
+
+
+@app.route("/account/risky/api/check", method="POST")
+def route_risky_api_check():
+    return {
+        "retcode": 0,
+        "message": "OK",
+        "data": {
+            "id": "",
+            "action": "ACTION_NONE",
+            "geetest": None
+        }
+    }
+
+
+@app.route("/bh3_usa/combo/granter/login/v2/login", method="POST")
+def route_login_v2():
+    return {
+        "message": "OK",
+        "retcode": 0,
+        "data":
+        {
+            "account_type": 1,
+            "combo_id": 0,
+            "combo_token": "token",
+            "data": {"guest": False},
+            "fatigue_remind": "null",
+            "heartbeat": "False",
+            "open_id": 1
+        }
+    }
+
+# Config Handlers
+
+
+@app.route("/bh3_usa/combo/granter/api/compareProtocolVersion", method="POST")
+def compareProtocolVersion():
+    return {
+        "retcode": 0,
+        "message": "OK",
+        "data": {
+            "modified": True,
+            "protocol": {
+                "id": 0,
+                "app_id": 7,
+                "language": "en",
+                "user_proto": "",
+                "priv_proto": "",
+                "major": 0,
+                "minimum": 4,
+                "create_time": "0",
+                "teenager_proto": "",
+                "third_proto": ""
+            }
+        }
+    }
+
+
+@app.route("/bh3_usa/mdk/shield/api/loadConfig")
+def route_mdk_shield_api_loadConfig():
+    return {
+        "retcode": 0,
+        "message": "OK",
+        "data": {
+            "id": 12,
+            "game_key": "bh3_usa",
+            "client": "PC",
+            "identity": "I_IDENTITY",
+            "guest": False,
+            "ignore_versions": "",
+            "scene": "S_NORMAL",
+            "name": "MoonlightBH",
+            "disable_regist": False,
+            "enable_email_captcha": False,
+            "thirdparty": ["tw"],
+            "disable_mmt": False,
+            "server_guest": False,
+            "thirdparty_ignore": {},
+            "enable_ps_bind_account": False,
+            "thirdparty_login_configs": {
+                "tw": {"token_type": "TK_GAME_TOKEN", "game_token_expires_in": 2592000},
+            },
+        },
+    }
+
+
+@app.route("/combo/box/api/config/sdk/combo")
+def route_config_sdk_combo():
+    return {
+        "retcode": 0,
+        "message": "OK",
+        "data": {
+            "vals": {
+                "kibana_pc_config": {
+                    "enable": 1,
+                    "level": "Debug",
+                    "modules": ["download"],
+                    "list_price_tierv2_enable": False,
+                    "network_report_config": {
+                        "enable": 1,
+                        "status_codes": [206],
+                        "url_paths": ["dataUpload", "red_dot"]
+                    }
+                }
+            }
+        }
+    }
+
+
+@app.route("/bh3_usa/combo/granter/api/getConfig")
+def route_combo_granter_api_getConfig():
+    return {
+        "retcode": 0,
+        "message": "OK",
+        "data": {
+            "protocol": True,
+            "qr_enabled": False,
+            "log_level": "DEBUG",
+            "announce_url": "http://localhost/announce",
+            "push_alias_type": 2,
+            "disable_ysdk_guard": False,
+            "enable_announce_pic_popup": False,
+        },
+    }
+
+
+@app.route('/admin/mi18n/plat_oversea/m2020030410/m2020030410-version.json')
+def route_2020030410_m2020030410():
+    return {"version": 67}
+
+
+@app.route("/device-fp/api/getExtList")
+def route_device_fp_api_getExtList():
+    return {
+        "retcode": 0,
+        "message": "OK",
+        "data": {
+            "code": 200,
+            "msg": "ok",
+            "ext_list": [
+                "cpuName",
+                "systemName",
+                "systemType",
+                "deviceUID",
+                "gpuID",
+                "gpuName",
+                "gpuAPI",
+                "gpuVendor",
+                "gpuVersion",
+                "gpuMemory",
+                "osVersion",
+                "cpuCores",
+                "cpuFrequency",
+                "gpuVendorID",
+                "isGpuMultiTread",
+                "memorySize",
+                "screenSize",
+                "engineName",
+                "addressMAC"
+            ],
+            "pkg_list": []
+        }
+    }
+
+# Dispatch Handlers
 
 
 @app.route("/query_dispatch")
@@ -69,13 +244,13 @@ def dispatch():
                     "res_use_asset_boundle": "0",
                     "show_bulletin_button": "0",
                     "show_bulletin_empty_dialog_bg": "0",
-                    "show_version_text": "1",
+                    "show_version_text": "0",
                     "update_streaming_asb": "0",
                     "use_multy_cdn": "0"
                 },
-                "name": "CrepeBH",
+                "name": "MoonlightBH",
                 "retcode": 0,
-                "title": "CrepeBH"
+                "title": "MoonlightBH"
             }
         ],
         "retcode": 0
@@ -125,7 +300,7 @@ def gateway():
         "is_data_ready": True,
         "msg": "",
         "oaserver_url": "http://localhost/oaserver",
-        "region_name": "CrepeBH",
+        "region_name": "MoonlightBH",
         "retcode": 0,
         "server_cur_time": 1670072875,
         "server_cur_timezone": 1,
@@ -136,16 +311,19 @@ def gateway():
     }
 
 
+# Index Handlers
+
 @app.route('/combo/box/api/config/sw/precache')
 def precache():
     return {"code": 0}
 
-@app.route('/crash/dataUpload',method="POST")
+
+@app.route('/crash/dataUpload', method="POST")
 def dataUpload():
     return {"code": 0}
 
 
-@app.route('/data_abtest_api/config/experiment/list',method="POST")
+@app.route('/data_abtest_api/config/experiment/list', method="POST")
 def experiment_list():
     return {
         "retcode": 0,
@@ -177,152 +355,11 @@ def getAgreementInfos():
     }
 
 
-@app.route("/bh3_usa/combo/granter/login/v2/login", method="POST")
-def route_login_v2():
-    return {
-        "message": "OK",
-        "retcode": 0,
-        "data":
-        {
-            "account_type": 1,
-            "combo_id": 0,
-            "combo_token": "token",
-            "data": {"guest": False},
-            "fatigue_remind": "null",
-            "heartbeat": "False",
-            "open_id": 1
-        }
-    }
-
-
-@app.route("/bh3_usa/mdk/shield/api/verify", method="POST")
-def route_mdk_vertify():
-    return ACCOUNT_INFO
-
-
-@app.route("/combo/box/api/config/sdk/combo")
-def route_config_sdk_combo():
-    return "{""retcode"":0,""message"":""OK"",""data"":{""vals"":{""kibana_pc_config"":""{ \"enable\": 1, \"level\": \"Debug\",\"modules\": [\"download\"]"",""list_price_tierv2_enable"":""false\n"",""network_report_config"":""{ \"enable\": 1, \"status_codes\": [206], \"url_paths\": [\"dataUpload\", \"red_dot\"] }""}}}"
-
-
-@app.route("/device-fp/api/getExtList")
-def route_config_sdk_combo():
-    return "{""retcode"":0,""message"":""OK"",""data"":{""code"":200,""msg"":""ok"",""ext_list"":[""cpuName"",""systemName"",""systemType"",""deviceUID"",""gpuID"",""gpuName"",""gpuAPI"",""gpuVendor"",""gpuVersion"",""gpuMemory"",""osVersion"",""cpuCores"",""cpuFrequency"",""gpuVendorID"",""isGpuMultiTread"",""memorySize"",""screenSize"",""engineName"",""addressMAC""],""pkg_list"":[]}}"
-
-
-@app.route("/account/risky/api/check", method="POST")
-def route_risky_api_check():
-    return {
-        "retcode": 0,
-        "message": "OK",
-        "data": {
-            "id": "",
-            "action": "ACTION_NONE",
-            "geetest": None
-        }
-    }
-
-
-@app.route("bh3_usa/mdk/shield/api/loadConfig", method="POST")
-def route_mdk_shield_api_loadConfig():
-    return {
-        "retcode": 0,
-        "message": "OK",
-        "data": {
-            "id": 12,
-            "game_key": "bh3_usa",
-            "client": "PC",
-            "identity": "I_IDENTITY",
-            "guest": False,
-            "ignore_versions": "",
-            "scene": "S_NORMAL",
-            "name": "CrepeBH",
-            "disable_regist": True,
-            "enable_email_captcha": False,
-            "thirdparty": ["fb", "tw"],
-            "disable_mmt": False,
-            "server_guest": False,
-            "thirdparty_ignore": {"tw": "", "fb": ""},
-            "enable_ps_bind_account": False,
-            "thirdparty_login_configs": {
-                "tw": {"token_type": "TK_GAME_TOKEN", "game_token_expires_in": 2592000},
-            },
-        },
-    }
-
-@app.route(
-    '/admin/mi18n/plat_oversea/m2020030410/m2020030410-version.json'
-)
-def route_20190628_5d15ba66cd922():
-    return {"version": 67}
-
-
-# @app.route("/admin/mi18n/plat_oversea/*#")
-# def route_plat_oversea():
-#     return {"version": 67}
-
-@app.route("/bh3_usa/combo/granter/api/compareProtocolVersion")
-def compareProtocolVersion():
-    return {
-    "retcode": 0,
-    "message": "OK",
-    "data": {
-        "modified": True,
-        "protocol": {
-            "id": 0,
-            "app_id": 7,
-            "language": "en",
-            "user_proto": "",
-            "priv_proto": "",
-            "major": 0,
-            "minimum": 4,
-            "create_time": "0",
-            "teenager_proto": "",
-            "third_proto": ""
-        }
-    }
-}
-
-
-@app.route("/bh3_usa/combo/granter/api/getConfig")
-def route_combo_granter_api_getConfig():
-    return {
-        "retcode": 0,
-        "message": "OK",
-        "data": {
-            "protocol": True,
-            "qr_enabled": False,
-            "log_level": "DEBUG",
-            "announce_url": "http://localhost/announce",
-            "push_alias_type": 2,
-            "disable_ysdk_guard": False,
-            "enable_announce_pic_popup": False,
-        },
-    }
-
-
-@app.route("/bh3_usa/combo/granter/api/getProtocol")
-def route_combo_granter_api_getProtocol():
-    return {
-        "retcode": 0,
-        "message": "OK",
-        "data": {
-            "modified": True,
-            "protocol": {
-                "id": 0,
-                "app_id": 7,
-                "language": "en",
-                "user_proto": "",
-                "priv_proto": "",
-                "major": 31,
-                "minimum": 0,
-                "create_time": "-",
-                "teenager_proto": "",
-                "third_proto": "",
-            },
-        },
-    }
+@app.route('/report', method='POST')
+def report():
+    return {"code": 0}
 
 
 def run_http_server(host):
-    app.run(host=host, port="81",silent=True)
+    print('[HTTP] Listening on http://127.0.0.1:80/')
+    app.run(host=host, port=80, quiet=True)
--- gameserver/handlers/auth.py
+++ gameserver/handlers/auth.py
@@ -1,8 +1,7 @@
 from gameserver.protocol.packet import Packet
 from gameserver.protocol.cmd_id import CmdId
 from gameserver import HandlerRouter,Connection
-from lib.proto import GetPlayerTokenReq,GetPlayerTokenRsp,PlayerLoginReq,PlayerLoginRsp,GetPlayerTokenRspRetcode
-
+from lib.proto import AccountType, CgType, GetPlayerTokenReq,GetPlayerTokenRsp,PlayerLoginReq,PlayerLoginRsp,GetPlayerTokenRspRetcode,UserType,PlayerLoginRspRetcode
 router = HandlerRouter()
 
 @router(CmdId.GetPlayerTokenReq)
@@ -10,15 +9,22 @@ def handle_token_req(conn: Connection, msg: GetPlayerTokenReq):
     rsp = GetPlayerTokenRsp(
         retcode= GetPlayerTokenRspRetcode.SUCC,
         uid=1,
-        account_type=24,
-        user_type=4,
+        is_bind_access=False,
         account_uid="1",
         hoyolab_account_uid="1",
-        token=msg.token,
+        account_type=AccountType.ACCOUNT_HOYOLAB,
+        user_type=UserType.USER_TYPE_CHANNEL
     )
     conn.send(rsp,False)
 
 @router(CmdId.PlayerLoginReq)
 def handle_login(conn: Connection, msg: PlayerLoginReq):
-    rsp = PlayerLoginRsp()
+    rsp = PlayerLoginRsp(
+        retcode=PlayerLoginRspRetcode.SUCC,
+        cg_type=CgType.CG_SEVEN_CHAPTER,
+        region_id=246,
+        region_name="MoonlightBH",
+        is_first_login=True,
+        login_session_token=50647798
+    )
     conn.send(rsp,False)
