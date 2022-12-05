81354ad682ace8f28759826e3c51d197b1d497b9
tamilpp25 <tamilpp25@gmail.com>
2022-12-06 00:35:55 +0530

    change importing
    ew timing why did you use this :skull:

--- gameserver/handlers/auth.py
+++ gameserver/handlers/auth.py
@@ -1,27 +1,27 @@
 from gameserver.protocol.packet import Packet
 from gameserver.protocol.cmd_id import CmdId
 from gameserver import HandlerRouter,Connection
-from lib.proto import AccountType, CgType, GetPlayerTokenReq,GetPlayerTokenRsp,PlayerLoginReq,PlayerLoginRsp,GetPlayerTokenRspRetcode,UserType,PlayerLoginRspRetcode
+import lib.proto as proto
 router = HandlerRouter()
 
 @router(CmdId.GetPlayerTokenReq)
-def handle_token_req(conn: Connection, msg: GetPlayerTokenReq):
-    rsp = GetPlayerTokenRsp(
-        retcode= GetPlayerTokenRspRetcode.SUCC,
+def handle_token_req(conn: Connection, msg: proto.GetPlayerTokenReq):
+    rsp = proto.GetPlayerTokenRsp(
+        retcode= proto.GetPlayerTokenRspRetcode.SUCC,
         uid=1,
         is_bind_access=False,
         account_uid="1",
         hoyolab_account_uid="1",
-        account_type=AccountType.ACCOUNT_HOYOLAB,
-        user_type=UserType.USER_TYPE_CHANNEL
+        account_type=proto.AccountType.ACCOUNT_HOYOLAB,
+        user_type=proto.UserType.USER_TYPE_CHANNEL
     )
     conn.send(rsp,False)
 
 @router(CmdId.PlayerLoginReq)
-def handle_login(conn: Connection, msg: PlayerLoginReq):
-    rsp = PlayerLoginRsp(
-        retcode=PlayerLoginRspRetcode.SUCC,
-        cg_type=CgType.CG_SEVEN_CHAPTER,
+def handle_login(conn: Connection, msg: proto.PlayerLoginReq):
+    rsp = proto.PlayerLoginRsp(
+        retcode=proto.PlayerLoginRspRetcode.SUCC,
+        cg_type=proto.CgType.CG_SEVEN_CHAPTER,
         region_id=246,
         region_name="MoonlightBH",
         is_first_login=True,
